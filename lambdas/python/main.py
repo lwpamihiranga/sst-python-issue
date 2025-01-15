976f30a269e17342e2128b768295e7199d31a565
Andy Cloke <apcloke@gmail.com>
2025-01-15 19:52:33 +0000

    add psycopg2

--- lambdas/python/main.py
+++ lambdas/python/main.py
@@ -1,6 +1,15 @@
 from sst import Resource
+import psycopg2
 
 def handler(evt, ctx):
     print('Hello from Python!')
-    print('Database database: ', Resource.BackendRds.database)
+    conn = psycopg2.connect(
+        host=Resource.BackendRds.host,
+        database=Resource.BackendRds.database,
+        user=Resource.BackendRds.username,
+        password=Resource.BackendRds.password,
+        port=Resource.BackendRds.port
+    )
+    print('Database connection: ', conn)
+    conn.close()
     return {"message": "Hello, World!"}
